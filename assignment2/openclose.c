28f8e09e5cd1488084f6c7ef5411f725425c8208
SmilesBlue <SmilesBlue@users.noreply.github.com>
2018-03-21 16:14:33 -0400

    Update openclose.c

--- assignment2/openclose.c
+++ assignment2/openclose.c
@@ -1,14 +1,15 @@
 #include <linux/fs.h>
-
 #include "h/init.h"
 #include "h/openclose.h"
 
 int device_open(struct inode *inode, struct file *file)
 {
+	printk(KERN_INFO "Device Opened\n");
 	return 0;
 }
 
 int device_release(struct inode *inode, struct file *file)
 {
+	printk(KERN_INFO "Device Released\n");
 	return 0;
 }
