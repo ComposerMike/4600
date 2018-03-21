32d10c7766502fb36cc32a4fbc8277ae4003a4ac
will <wyoung348@gmail.com>
2018-03-21 13:46:45 -0400

    hello world

--- assignment2/h/init.h
+++ assignment2/h/init.h
@@ -1,7 +1,7 @@
 #ifndef INIT_H
 #define INIT_H
 
-int init();
-int deinit();
+int init_module(void);
+void cleanup_module(void);
 
 #endif // INIT_H
--- assignment2/init.c
+++ assignment2/init.c
@@ -1,18 +1,16 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
 #include "h/init.h"
 
-/*
-int init()
+int init_module()
 {
-}
-*/
+	printk(KERN_INFO "Hello world 1.\n");
 
-/*
-int deinit()
-{
-}
-*/
-
-int main()
-{
 	return 0;
+}
+
+void cleanup_module()
+{
+	printk(KERN_INFO "Goodbye world 1.\n");
 }
